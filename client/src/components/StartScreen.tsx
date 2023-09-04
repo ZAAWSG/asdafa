1530049dfa162008b6c325b1576ed30eb7ccf662
Asylkhan Sagyndyk <asylkhan_sagyndyk@akta.tech>
2023-09-04 18:43:59 +0600

    https on

--- client/src/components/StartScreen.tsx
+++ client/src/components/StartScreen.tsx
@@ -15,7 +15,7 @@ const Container = styled.div`
 class StartScreen extends React.Component {
   startGame = async () => {
     try {
-      const http = window.location.host === 'localhost:3000' ? 'http' : 'http';
+      const http = window.location.host === 'localhost:3000' ? 'http' : 'https';
       const url= `${http}://${window.location.host}/api/start-game`;
       const res = await fetch(url);
       const resJson = await res.json();
